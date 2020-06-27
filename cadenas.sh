f9b5ddeb4a5c6d2608c4964a48fc8d38302af414
Automecanic <albertofernandez@Tremenda.local>
2020-06-27 16:18:03 +0200

    utima actualización

--- cadenas.sh
+++ cadenas.sh
@@ -3,6 +3,8 @@
 #diremos cadenas o Strings al texto en general dentro de una variable.
 cadena="Estaba el señor dongato, sentadito en su tejado"
 cadena2="antonia"
+cadena3=${cadena:6:17}
+
 
 #Mayusculas minusculas  no me funciona en bash  3.2
 #Para actualizar el bash en catalina no es possible  por el tema de licencias
@@ -22,3 +24,11 @@ echo  ${cadena,} #pone la primera en minuscula todas en mayusculas.
 #Salida: estaba el señor.....
 echo ${cadena2^^} #pone todas en mayusculas 
 echo  ${cadena,,} #pone tosa en minuscula todas en mayusculas.
+echo $cadena3 #imprime el substring  el  señor don gato
+echo ${cadena/e/j}  #substituye la primera e por j
+echo ${cadena//e/j}  #substituye todas las  "e" minusculas  por j
+
+echo ${cadena#*a} #imprime desde la primera a
+echo ${cadena##*a}#imprime desce la ultima a
+echo ${cadena%a*} #imprime asta la ultima a
+echo ${cadena%%a*} #imprime asta la primera a
