a89b16de9763cd7e670078bf415fbb0cf5c9cd69
Automecanic <albertofernandez@Tremenda.local>
2020-06-27 16:18:53 +0200

    Utima actualización

--- cadenas.sh
+++ cadenas.sh
@@ -23,12 +23,12 @@ echo  ${cadena,} #pone la primera en minuscula todas en mayusculas.
 #Salida:  Antonia
 #Salida: estaba el señor.....
 echo ${cadena2^^} #pone todas en mayusculas 
-echo  ${cadena,,} #pone tosa en minuscula todas en mayusculas.
+echo  ${cadena,,} #pone todo en minuscula todas en mayusculas.
 echo $cadena3 #imprime el substring  el  señor don gato
 echo ${cadena/e/j}  #substituye la primera e por j
 echo ${cadena//e/j}  #substituye todas las  "e" minusculas  por j
 
 echo ${cadena#*a} #imprime desde la primera a
 echo ${cadena##*a}#imprime desce la ultima a
-echo ${cadena%a*} #imprime asta la ultima a
-echo ${cadena%%a*} #imprime asta la primera a
+echo ${cadena%a*} #imprime hasta la ultima a
+echo ${cadena%%a*} #imprime hasta la primera a
